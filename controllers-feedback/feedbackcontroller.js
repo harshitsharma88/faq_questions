342cb025b5656cf19bbd3ad91da96bc317e9a79f
harshitcultureholidays <harshit.cultureholidays@gmail.com>
2025-01-06 10:18:40 +0530

    Changed Unreplied Questions Fetching

--- controllers-feedback/feedbackcontroller.js
+++ controllers-feedback/feedbackcontroller.js
@@ -1,34 +1,41 @@
 const catchBlock = require("../utils/errorHandler");
 let { sql } = require('../dbConnection/sqlConnect');
-const { executeStoredProcedure, executeQuery } = require("../dbConnection/executingQuery");
+const { executeStoredProcedure } = require("../dbConnection/executingQuery");
 
 async function getFeedbackQuestions(req, res, next){
     try {
         const {agentid, page} = req.body;
-        // const agentid = "CHAGT123";
-        // const page = "Zodiac";
-        const repliedQuestions = {};
-        const questionParamArray = [
-            {name : "STATUS" , type : sql.Bit, value : 1},
-            {name : "PAGE" , type : sql.NVarChar(100), value : page}
-        ];
-        const responseParamArray = [
+        const paramArray = [
             {name : "AGENTID" , type : sql.NVarChar(50), value : agentid},
             {name : "PAGE" , type : sql.NVarChar(100), value : page}
         ];
-        const feedbackQuestionsPromise = executeStoredProcedure("FEEDBACK_GET_ALL_QUESTIONS", questionParamArray);
-        const responsesPromise = executeStoredProcedure("FEEDBACK_GET_AGENT_RESPONSES", responseParamArray);
+        const result = await executeStoredProcedure("FEEDBACK_GET_UNREPLIED_QUESTION_BY_PAGE", paramArray);
+        console.log(result);
+        return res.status(200).json(result)
+        // // const agentid = "CHAGT123";
+        // // const page = "Zodiac";
+        // const repliedQuestions = {};
+        // const questionParamArray = [
+        //     {name : "STATUS" , type : sql.Bit, value : 1},
+        //     {name : "PAGE" , type : sql.NVarChar(100), value : page}
+        // ];
+        // const responseParamArray = [
+        //     {name : "AGENTID" , type : sql.NVarChar(50), value : agentid},
+        //     {name : "PAGE" , type : sql.NVarChar(100), value : page}
+        // ];
+        // const feedbackQuestionsPromise = executeStoredProcedure("FEEDBACK_GET_ALL_QUESTIONS", questionParamArray);
+        // const responsesPromise = executeStoredProcedure("FEEDBACK_GET_AGENT_RESPONSES", responseParamArray);
 
-        const [{value:questions},{value:responses}] = await Promise.allSettled([feedbackQuestionsPromise, responsesPromise]);
-        responses.forEach(response=>{
-            repliedQuestions[response.feed_qstn_id] = true
-        });
-        const allAvailableQuestions = [];
-        questions.forEach((question => {
-            if(!repliedQuestions[question.feed_qstn_id] && question.status) 
-                allAvailableQuestions.push({qstnid : question.feed_qstn_id, title : question.feed_qstn_title});
-            }));
-        return res.status(200).json(allAvailableQuestions)
+        // const [{value:questions},{value:responses}] = await Promise.allSettled([feedbackQuestionsPromise, responsesPromise]);
+        // responses.forEach(response=>{
+        //     repliedQuestions[response.feed_qstn_id] = true
+        // });
+        // const allAvailableQuestions = [];
+        // questions.forEach((question => {
+        //     if(!repliedQuestions[question.feed_qstn_id] && question.status) 
+        //         allAvailableQuestions.push({qstnid : question.feed_qstn_id, title : question.feed_qstn_title});
+        //     }));
+        // return res.status(200).json(allAvailableQuestions)
     } catch (error) {
         catchBlock(error, "Getting Feedback Questions", res)
     }
@@ -36,7 +43,6 @@ async function getFeedbackQuestions(req, res, next){
 
 async function storeNewFeedbackQuestion(req, res, next){
     try {
-        console.log(req.body);
         const {title, page} = req.body;
         const userid = req.user || "admin";
         const paramArray = [
@@ -44,7 +50,6 @@ async function storeNewFeedbackQuestion(req, res, next){
             {name : "PAGE", type: sql.NVarChar(100), value : page},
             {name : "CREATEDBY", type: sql.NVarChar(50), value : userid},
             {name : "CREATEDAT", type: sql.DateTime, value : new Date()},
-
         ]
         const result = await executeStoredProcedure("FEEDBACK_STORE_NEW_QUESTION", paramArray);
         return res.status(200).json(result);
@@ -56,32 +61,37 @@ async function storeNewFeedbackQuestion(req, res, next){
 async function storeAgentResponse(req, res, next){
     try {
         const {agentid, responsetext, rating, qstnid} = req.body;
-        const incrementSPArray = [
-            {name : "INCREASEVALUE", type : sql.Bit, value :1},
-            {name : "QSTIND", type : sql.Int, value :qstnid}
-        ]
-        const questionIncrement = await executeStoredProcedure("FEEDBACK_UPDATE_QUESTION_DETAILS",incrementSPArray);
-        if(Array.isArray(questionIncrement) && questionIncrement.length > 0){
             const paramArray = [
                 {name : "AGENTID", type : sql.NVarChar(50), value : agentid},
                 {name : "QSTNID", type : sql.Int, value : qstnid},
                 {name : "CREATEDAT", type : sql.DateTime, value : new Date()},
                 {name : "RESPONSETEXT", type : sql.NVarChar(1000), value : responsetext},
-                {name : "QSTNTITLE", type : sql.NVarChar(1000), value : questionIncrement[0].feed_qstn_title},
-                {name : "RATING", type : sql.Int, value : rating},
-                {name : "PAGE", type : sql.NVarChar(100), value : questionIncrement[0].page}
+                {name : "RATING", type : sql.Int, value : rating}
             ]            
             const result = await executeStoredProcedure("FEEDBACK_STORE_AGENT_RESPONSE", paramArray);
             return res.status(200).json(result);
-        }
-        return res.status(200).json("Not Stored");
     } catch (error) {
         catchBlock(error, "Storing Agent Response", res)
+    }
+}
+
+async function getAllFeedbackResponses(req, res, next){
+    try {
+        const {page, qstnid} = req.body;
+        const paramArray = [
+            {name : "PAGE", type : sql.NVarChar(100), value : page}
+        ]
+        const result = await executeStoredProcedure("",paramArray);
+        console.log(result);
+        return res.status(200)
+    } catch (error) {
+        catchBlock(error, "Getting All FeedBack responses", res)
     }
 }
 
 module.exports = {
     getFeedbackQuestions,
     storeAgentResponse,
-    storeNewFeedbackQuestion
+    storeNewFeedbackQuestion,
+    getAllFeedbackResponses
 }
--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,11 +1,13 @@
 const Router = require('express').Router();
 const { storeAgentResponse, storeNewFeedbackQuestion, 
-        getFeedbackQuestions} = require('../controllers-feedback/feedbackcontroller');
+        getFeedbackQuestions, getAllFeedbackResponses} = require('../controllers-feedback/feedbackcontroller');
 
 Router.post("/getpageqstn", getFeedbackQuestions);
 
 Router.post("/storeqstn", storeNewFeedbackQuestion);
 
 Router.post("/storeresponse", storeAgentResponse);
+
+Router.get("/getresponse", getAllFeedbackResponses)
 
 module.exports =  Router;
